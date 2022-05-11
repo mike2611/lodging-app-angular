8f00fddec11039b762ca9bdc856151a7869a6011
mike2611 <mpuentes2611@gmail.com>
2022-05-10 22:12:07 -0500

    Add styles to components

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     LoginRoutingModule,
     ReactiveFormsModule,
+    MaterialModule,
   ]
 })
 export class LoginModule { }
