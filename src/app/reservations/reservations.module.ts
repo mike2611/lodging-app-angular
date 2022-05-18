cdca4f7e72267a35ba53259fa4fe71272a78fed1
mike2611 <mpuentes2611@gmail.com>
2022-05-18 11:47:10 -0500

    Add basic structure to reservations component

--- src/app/reservations/reservations.module.ts
+++ src/app/reservations/reservations.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationsComponent } from './reservations.component';
 import { ReservationsRoutingModule } from './reservations-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -9,6 +10,7 @@ import { ReservationsRoutingModule } from './reservations-routing.module';
   imports: [
     CommonModule,
     ReservationsRoutingModule,
+    MaterialModule,
   ]
 })
 export class ReservationsModule { }
